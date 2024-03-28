7e9d3793fd0d0f6d72c21a2805c1c64d314c88c9
fionae <165266867+karafionae@users.noreply.github.com>
2024-03-28 16:36:10 +0700

    Update run.sh

--- run.sh
+++ run.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 
 
-curl_output=$(curl -H "Content-type: application/json" -X POST --data '{"jsonrpc":"2.0","method":"eth_getBlockByNumber","params":["latest", false],"id":1}' https://RPCETHLAVA/)
+curl_output=$(curl -H "Content-type: application/json" -X POST --data '{"jsonrpc":"2.0","method":"eth_getBlockByNumber","params":["latest", false],"id":1}' https://eth1.lava.build/lava-referer-6717d072-d710-4ed5-be92-14cf75ce8756/)
 
 
 echo "$curl_output" >> Update.tsx
